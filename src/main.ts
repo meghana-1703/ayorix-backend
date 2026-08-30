7f5ee98466a440cbf1d9498b367790cfd41ca951
Muduthanapally Meghana <m.meghana1225@gmail.com>
2026-08-30 14:41:45 +0530

    fix production CORS for AIRA

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: [
-      'http://localhost:3000',
-    ],
-    credentials: true,
-  });
+ app.enableCors({
+  origin: [
+    "https://www.ayorix.in",
+    "https://ayorix.in",
+  ],
+  credentials: true,
+});
 
   await app.listen(process.env.PORT ?? 3001);
 }
